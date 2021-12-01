e29e23d99ef8c9f4e4f9b2e62d52ed0b5a940bd6
Alex.link <alex.link_mail@e1.ru>
2021-12-01 23:54:11 +0500

    Example 012 Methods +

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -84,6 +84,88 @@ string Method4(int count, string text)
 }
 
 string res = Method4(10, "z");
-Console.WriteLine(res);
+//Console.WriteLine(res);
 
 
+// for (int i = 2; i <= 10; i++)
+// {
+//     for (int j = 2; j <= 10; j++)
+//     {
+//         Console.WriteLine($"{i} x{j} = {i * j}");
+//     }
+//     Console.WriteLine();
+// }
+
+
+//===== Работа с текстом
+// // // Дан текст. В тексте нужно все пробелы заменить черточками, 
+// // // маленькие буквы “к” заменить большими “К”,
+// // // a большие “С” заменить маленькими “с”.
+
+// // // Ясна ли задача?
+
+string text = "— Я думаю, — сказал князь, улыбаясь, — что, "
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля. "
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+
+// string s = "qwerty"
+//             012
+// s[3] // r
+
+string Replace(string texT, char oldValue, char newValue)
+{
+    string result = String.Empty;
+
+    int length = texT.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (texT[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{texT[i]}";
+    }
+
+    return result;
+}
+
+// string newText = Replace(text, ' ', '|');
+
+// Console.WriteLine(newText);
+// Console.WriteLine();
+// newText = Replace(newText, 'к', 'К');
+// Console.WriteLine(newText);
+
+
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition]) minPosition = j;
+        }
+        int tempopary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = tempopary;
+    }
+}
+
+
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
